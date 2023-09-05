29993af30647ec17f20aea9aeee90ba665af0d46
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-09-06 02:54:49 +0500

    m

--- app/page.js
+++ app/page.js
@@ -1,57 +1,40 @@
 "use client"
-import { useState } from "react";
-import axios from 'axios';
+
+import React , { useState } from "react";
+
 
 export default function Home() {
-  // const [data, setData] = useState([]);
-  // const [fol, setFol] = useState([]);
-
-  // const loadDataFromServer = async () => {
-  //   try {
-  //     const response = await axios.get('https://api.github.com/users/Ghullam-Mustafa/followers');
-  //     setData(response.data);
-  //   } catch (error) {
-  //     console.error("Error loading followers:", error);
-  //   }
-  // }
-
-  // const loadDataFromServers = async () => {
-  //   try {
-  //     const following = await axios.get('https://api.github.com/users/Ghullam-Mustafa/following');
-  //     setFol(following.data); // Use following.data to set the state
-  //   } catch (error) {
-  //     console.error("Error loading following:", error);
-  //   }
-  // }
+ 
 
   const [data, setData] = useState([])
+  const [userName, setUserName] = useState("")
+
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleGet = async () => {
+    try {
+      const link = await fetch(`https://api.github.com/users/${userName}`, {
+        method:"GET"
+      });
+      if (!link.ok) {
+        alert("User not found");
+        setUserName('')
+        return
+      }
+      const userData = await link.json();
+      console.log(userData);
+      return
+      setData([...data, userData]);
+      setUserName('')
+    } catch (error) {
+     console.error(error);
+    }
+  };
 
   return (
     <>
-      <div className={true ? 'red' : 'black'}>
-        {data.map((item, a) => {
-          return (
-            <div key={a}>
-              Follower login - {item.login}
-            </div>
-          )
-        })}
-        <button onClick={loadDataFromServer}>Load Followers</button>
-      </div>
-
-      <div className={true ? 'blue' : 'orange'}>
-        {fol.map((value, i) => {
-          return (
-            <div key={i} >
-              Following login - {value.login}
-            </div>
-          )
-        })}
-        <button onClick={loadDataFromServers}>Load Following</button>
-      </div>
-
-
-
       <div className="bg-[#D6EFFF] py-10 ">
         <div className=" max-w-[1320px] mx-auto md:py-[80] py-5  ">
           <div className="text-[#33B1FF] font-mono text-3xl text-center">Github User Finder</div>
@@ -64,23 +47,43 @@ export default function Home() {
               onChange={handleChange}
             />
           </div>
-          <div className="text-center"><button className="bg-[#33B1FF] hover:bg-[#1FA9FF] text-white font-bold py-2 px-4 rounded">
-            Click me
+          <div className="text-center"><button onClick={handleGet} className="bg-[#33B1FF] hover:bg-[#1FA9FF] text-white font-bold py-2 px-4 rounded">
+            Get Data
           </button>
           </div>
 
           <table className="my-5">
-            <thead>
+            <thead className="border-b font-medium  border-[#33B1FF]">
               <tr className="">
-                <td className="text-[#33B1FF] px-10">#</td>
-                <td className="text-[#33B1FF] px-20">Profile</td>
-                <td className="text-[#33B1FF] px-20">Name</td>
-                <td className="text-[#33B1FF] px-20">Followers</td>
-                <td className="text-[#33B1FF] px-20">Following</td>
-                <td className="text-[#33B1FF] px-20">Github</td>
-                <td className="text-[#33B1FF] px-10">Action</td> <hr />
+                <th className="text-[#33B1FF] py-10 px-10">#</th>
+                <th className="text-[#33B1FF] py-10 px-20">Profile</th>
+                <th className="text-[#33B1FF] py-10 px-20">Name</th>
+                <th className="text-[#33B1FF] py-10 px-20">Followers</th>
+                <th className="text-[#33B1FF] py-10 px-20">Following</th>
+                <th className="text-[#33B1FF] py-10 px-20">Public repos</th>
+                <th className="text-[#33B1FF] py-10 px-20">Github</th>
+                <th className="text-[#33B1FF] py-10 px-10">Action</th>
               </tr>
             </thead>
+            {data.map((item, i) => {
+              {/* return <> */}
+              <div className="text-black text-3xl"> dKJJJJJJJJJJJJJ</div>
+                   {/* </> */}
+
+              {/* // <tr key={i} className="border-b  border-[#33B1FF]"> */}
+              {/* //   <td className="whitespace-nowrap px-6 py-4 font-medium">{i + 1}</td>
+              //   <td className="whitespace-nowrap px-6 py-4 font-medium">
+              //     <img src={item.avatar_url} alt="profile picture" width={70} height={70} className="rounde-full" />
+              //   </td>
+              //   <td className="whitespace-nowrap px-6 py-4 font-medium">{item.login} sjwwkdjhdie</td>
+              //   <td className="whitespace-nowrap px-6 py-4 font-medium">{item.followers} jodiojfofjojfoj</td>
+              //   <td className="whitespace-nowrap px-6 py-4 font-medium">{item.following}</td>
+              //   <td className="whitespace-nowrap px-6 py-4 font-medium">{item.public_repos}</td>
+              //   <td>
+              //     <a href={item.html_url} target='blank' className="whitespace-nowrap px-6 py-4">{item.html_url}</a>
+              //   </td> */}
+              {/* // </tr> */}
+           })} 
           </table>
         </div>
       </div>
